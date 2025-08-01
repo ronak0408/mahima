0139fedea241f68de2e1463aaa0e5ff5db18b831
Akshay Yadav <122807716+imakshayyadavv@users.noreply.github.com>
2025-08-01 16:39:13 +0530

    Update DhamMahima.jsx

--- src/Pages/DhamMahima.jsx
+++ src/Pages/DhamMahima.jsx
@@ -12,20 +12,18 @@ export default function DhamMahima() {
         <div
           className="absolute inset-0 bg-cover bg-center"
           style={{
-            backgroundImage:
-              "url('https://s7ap1.scene7.com/is/image/incredibleindia/2-mana-village-badrinath-uttarakhand-attr-hero?qlt=82&ts=1726646481060')",
+      
           }}
         ></div>
 
         {/* Overlay */}
-        <div className="absolute inset-0 bg-orange-400/60 "></div>
+        {/* <div className="absolute inset-0 bg-orange-400/60 "></div> */}
 
         {/* Content */}
-        <div className="relative z-10 p-4 md:p-8">
-          <h1 className="text-center text-2xl md:text-4xl font-semibold mt-16">
-            Visit Most Famous Dhams
-          </h1>
+        <div className="relative z-10 ">
+         
           <div className="mt-1 w-full">
+             
             <DhamGrid />
           </div>
         </div>
